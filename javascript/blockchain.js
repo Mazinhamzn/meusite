d632806af3ec4c46c9a4aeb4e660020084cb3790
Mazinhamzn <56055742+Mazinhamzn@users.noreply.github.com>
2019-11-21 21:05:55 -0300

    Add files via upload

--- javascript/blockchain.js
+++ javascript/blockchain.js
@@ -1,4 +1,4 @@
-var enderecoContrato = "0x53C34f4897a4111f9299a9b734aD1d5c380a8Ce5";
+var enderecoContrato = "0xffb001ee5cb2c83224544008bbbe89d2871600d6";
 var provedor = new ethers.providers.Web3Provider(web3.currentProvider);
 ethereum.enable();
 var signatario = provedor.getSigner();
